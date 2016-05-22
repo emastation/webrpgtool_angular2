d9c746c88ac7a30bd5abe57975c79705d9671134
Yuki Shimada <yukiema@gmail.com>
2016-05-22 11:34:45 +0900

    Use "@angular/router-deprecated"

--- 01_Dev/client/app.component.ts
+++ 01_Dev/client/app.component.ts
@@ -1,12 +1,27 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { APP_BASE_HREF } from '@angular/common';
+import { SiteTopComponent } from './imports/site-top/site-top.component';
 
+@RouteConfig([
+  {
+    path: '/',
+    as: 'SiteTop',
+    component: SiteTopComponent,
+    useAsDefault: true
+  }
+])
 @Component({
   selector: 'app',
-  templateUrl: 'client/app.component.html'
+  templateUrl: 'client/app.component.html',
+  directives: [ROUTER_DIRECTIVES]
 })
 class AppComponent { }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  provide(APP_BASE_HREF, { useValue: '/' })
+]);
---
+++ 01_Dev/client/imports/site-top/site-top.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'site-top',
+  templateUrl: 'client/imports/site-top/site-top.component.html',
+  directives: []
+})
+export class SiteTopComponent {
+}
